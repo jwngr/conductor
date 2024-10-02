0bd65697e03c9a0de9072ccb0518ddae323fdf36
jwngr <wenger.jacob@gmail.com>
2024-10-01 20:13:42 -0700

    Remove config import

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -6,6 +6,3 @@ export * as types from './types';
 // Export lib modules
 // export * as firebase from './lib/firebase';
 export * as importQueue from './lib/importQueue';
-
-// Export config
-export * as config from './config';
