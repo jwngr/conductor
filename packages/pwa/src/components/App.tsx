478a1e2255b9b8eeb0c5d4abc1eb3c7f147b4c8a
jwngr <wenger.jacob@gmail.com>
2024-10-11 23:06:44 -0700

    More layout improvements with better Flex component

--- packages/pwa/src/components/App.tsx
+++ packages/pwa/src/components/App.tsx
@@ -6,29 +6,42 @@ import {HomeScreen} from '@src/screens/HomeScreen';
 import {SavedScreen} from '@src/screens/SavedScreen';
 import React from 'react';
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
-import {ThemeProvider} from 'styled-components';
+import styled, {ThemeProvider} from 'styled-components';
 
 import {AppHeader} from './AppHeader';
-import {FlexRow} from './atoms/Flex';
+import {FlexColumn, FlexRow} from './atoms/Flex';
 import {LeftSidebar} from './LeftSidebar';
+
+const AppWrapper = styled(FlexColumn)`
+  height: 100%;
+  width: 100%;
+`;
+
+const MainContentWrapper = styled.div`
+  flex: 1;
+`;
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <AppHeader />
-        <FlexRow align="flex-start">
-          <LeftSidebar />
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/saved" element={<SavedScreen />} />
-            <Route path="/done" element={<DoneScreen />} />
-            <Route path="/all" element={<AllScreen />} />
-            <Route path="/items/:feedItemId?/" element={<FeedItemScreen />} />
-            {/* Redirect unmatched routes to home page, replacing history stack. */}
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </FlexRow>
+        <AppWrapper>
+          <AppHeader />
+          <FlexRow align="flex-start" flex>
+            <LeftSidebar />
+            <MainContentWrapper>
+              <Routes>
+                <Route path="/" element={<HomeScreen />} />
+                <Route path="/saved" element={<SavedScreen />} />
+                <Route path="/done" element={<DoneScreen />} />
+                <Route path="/all" element={<AllScreen />} />
+                <Route path="/items/:feedItemId?/" element={<FeedItemScreen />} />
+                {/* Redirect unmatched routes to home page, replacing history stack. */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </MainContentWrapper>
+          </FlexRow>
+        </AppWrapper>
       </BrowserRouter>
     </ThemeProvider>
   );
