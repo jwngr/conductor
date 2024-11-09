8b4b762947af25fd580ab19dde58f1a97a952250
jwngr <wenger.jacob@gmail.com>
2024-11-08 19:51:39 -0800

    Simpler errors code

--- packages/shared/src/lib/errors.ts
+++ packages/shared/src/lib/errors.ts
@@ -1,38 +1,58 @@
 import {Func, Supplier} from '@shared/types/utils.types';
 
-interface TryWithErrorMessageArgs<T> {
-  readonly fn: Supplier<T>;
+interface BaseTryWithErrorMessageArg<T> {
   readonly onError: Func<Error, T>;
   readonly errorMessagePrefix?: string;
 }
 
+interface SyncTryWithErrorMessageArgs<T> extends BaseTryWithErrorMessageArg<T> {
+  readonly fn: Supplier<T>;
+}
+
+interface AsyncTryWithErrorMessageArgs<T> extends BaseTryWithErrorMessageArg<T> {
+  readonly asyncFn: Supplier<Promise<T>>;
+}
+
+/**
+ * Handles an error by creating a better error message and calling `onError` with it.
+ */
+function handleError<T>(error: unknown, onError: Func<Error, T>, errorMessagePrefix?: string): T {
+  let betterError: Error;
+  const prefix = errorMessagePrefix ? `${errorMessagePrefix}: ` : '';
+  if (error instanceof Error) {
+    betterError = new Error(`${prefix}${error.message}`, {cause: error});
+  } else {
+    const defaultErrorMessage = 'An unexpected error occurred';
+    betterError = new Error(`${prefix}${error ?? defaultErrorMessage}`);
+  }
+
+  return onError(betterError);
+}
+
+/**
+ * Executes the given synchronous function and returns its result. If an error is thrown, it is
+ * caught and returned as an error with a better message.
+ *
+ * For asynchronous functions, see {@link asyncTryWithErrorMessage}.
+ */
 export function tryWithErrorMessage<T>({
   fn,
   onError,
   errorMessagePrefix,
-}: TryWithErrorMessageArgs<T>): T {
+}: SyncTryWithErrorMessageArgs<T>): T {
   try {
     return fn();
   } catch (error) {
-    let betterError: Error;
-    const prefix = errorMessagePrefix ? `${errorMessagePrefix}: ` : '';
-    if (error instanceof Error) {
-      betterError = new Error(`${prefix}${error.message}`, {cause: error});
-    } else {
-      const defaultErrorMessage = 'An unexpected error occurred';
-      betterError = new Error(`${prefix}${error ?? defaultErrorMessage}`);
-    }
-
-    return onError(betterError);
+    return handleError(error, onError, errorMessagePrefix);
   }
 }
 
-interface AsyncTryWithErrorMessageArgs<T> {
-  readonly asyncFn: Supplier<Promise<T>>;
-  readonly onError: Func<Error, T>;
-  readonly errorMessagePrefix?: string;
-}
-
+/**
+ * Executes the given asynchronous function and returns its result. If an error is thrown, it is
+ * caught and returned as an error with a better message.
+ *
+ * For synchronous functions, see {@link tryWithErrorMessage}.
+ */
 export async function asyncTryWithErrorMessage<T>({
   asyncFn,
   onError,
@@ -41,15 +61,6 @@ export async function asyncTryWithErrorMessage<T>({
   try {
     return await asyncFn();
   } catch (error) {
-    let betterError: Error;
-    const prefix = errorMessagePrefix ? `${errorMessagePrefix}: ` : '';
-    if (error instanceof Error) {
-      betterError = new Error(`${prefix}${error.message}`, {cause: error});
-    } else {
-      const defaultErrorMessage = 'An unexpected error occurred';
-      betterError = new Error(`${prefix}${error ?? defaultErrorMessage}`);
-    }
-
-    return onError(betterError);
+    return handleError(error, onError, errorMessagePrefix);
   }
 }
