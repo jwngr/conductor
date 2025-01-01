c36a7acc3ee38991cb9b5c6872dfe9b072cd97be
jwngr <wenger.jacob@gmail.com>
2024-12-31 19:39:26 -0800

    Added global handler

--- packages/pwa/src/index.tsx
+++ packages/pwa/src/index.tsx
@@ -4,11 +4,15 @@ import '@src/index.css';
 
 import {App} from '@src/components/App.pwa';
 
+import {setupGlobalErrorHandlers} from '@src/lib/error.pwa';
+
 const rootDiv = document.getElementById('root');
 if (!rootDiv) {
   // eslint-disable-next-line no-restricted-syntax
   throw new Error('Root element not found');
 }
+
+setupGlobalErrorHandlers();
 
 const root = createRoot(rootDiv);
 root.render(<App />);
