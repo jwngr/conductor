7032e9e0a41a8f2287b2ec80b339fbf83516836a
Jacob Wenger <wenger.jacob@gmail.com>
2025-01-11 15:51:47 -0800

    Fixed error being thrown when signing out (#136)

--- packages/pwa/src/components/devToolbar/RegisterUserFeedSubscriberDevToolbarActions.tsx
+++ packages/pwa/src/components/devToolbar/RegisterUserFeedSubscriberDevToolbarActions.tsx
@@ -52,6 +52,7 @@ export const RegisterAccountFeedSubscriberDevToolbarSection: React.FC = () => {
       sectionType: DevToolbarSectionType.AccountFeedSubscriber,
       title: 'Account feed subscriber',
       renderSection: () => <AccountFeedSubscriber />,
+      requiresAuth: true,
     });
   }, [registerSection]);
 
