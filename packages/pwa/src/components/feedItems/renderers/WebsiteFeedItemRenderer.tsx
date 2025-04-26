c9dc3cce27712380d940d0b0f15c916cd77f8897
Jacob Wenger <wenger.jacob@gmail.com>
2025-04-26 16:08:32 -0700

    Reframed to feed item renderers (#202)

--- packages/pwa/src/components/feedItems/renderers/WebsiteFeedItemRenderer.tsx
+++ packages/pwa/src/components/feedItems/renderers/WebsiteFeedItemRenderer.tsx
@@ -2,13 +2,13 @@ import type React from 'react';
 
 import {SharedFeedItemHelpers} from '@shared/lib/feedItems.shared';
 
-import type {ArticleFeedItem} from '@shared/types/feedItems.types';
+import type {WebsiteFeedItem} from '@shared/types/feedItems.types';
 
 import {FeedItemHeader, FeedItemSummary, FeedItemWrapper} from '@src/components/feedItems/FeedItem';
 import {FeedItemMarkdown} from '@src/components/feedItems/FeedItemMarkdown';
 import {ImportingFeedItem} from '@src/components/feedItems/ImportingFeedItem';
 
-export const ArticleFeedItemComponent: React.FC<{readonly feedItem: ArticleFeedItem}> = ({
+export const WebsiteFeedItemRenderer: React.FC<{readonly feedItem: WebsiteFeedItem}> = ({
   feedItem,
 }) => {
   const hasFeedItemEverBeenImported = SharedFeedItemHelpers.hasEverBeenImported(feedItem);
