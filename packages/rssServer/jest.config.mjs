fe3fdcda232feaee8caec0317413aad9b72e3850
jwngr <wenger.jacob@gmail.com>
2025-05-14 22:15:19 -0700

    fix

--- packages/rssServer/jest.config.mjs
+++ packages/rssServer/jest.config.mjs
@@ -4,9 +4,8 @@ import baseConfig from '../../jest.base.config.mjs';
 export default {
   ...baseConfig,
   moduleNameMapper: {
-    '^@rssServer/(.*)$': '<rootDir>/src/$1',
+    '^@src/(.*)$': '<rootDir>/src/$1',
     '^@shared/(.*)$': '<rootDir>/../shared/src/$1',
     '^@sharedServer/(.*)$': '<rootDir>/../sharedServer/src/$1',
   },
-  testEnvironment: 'node',
 };
