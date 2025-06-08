9efdea18665f3dc1d313f1445607af8acf73c5a8
jwngr <wenger.jacob@gmail.com>
2025-06-07 20:25:23 -0700

    Removing unused stuff

--- packages/functions/src/reqHandlers/handleSuperfeedrWebhook.ts
+++ packages/functions/src/reqHandlers/handleSuperfeedrWebhook.ts
@@ -1,5 +1,4 @@
-// Import types from express instead
-import type {Request} from 'express';
+import type {Request} from 'firebase-functions/v2/https';
 
 import {logger} from '@shared/services/logger.shared';
 
